de6cef2cf3cdb81cdd20ace38ece88c437e7fda7
Tomasz Trzcinski <tomasz.trzcinski@capgemini.com>
2018-08-17 12:58:34 +0200

    Added pancakes prompt.

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -13,37 +13,67 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
     {
         protected int count = 1;
 
+        protected string askedAboutPancakes = null;
+
         public async Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
         }
 
-        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
         {
             var message = await argument;
 
-            if (message.Text == "reset")
+            string response = null;
+            
+            switch (message.Text)
             {
-                PromptDialog.Confirm(
-                    context,
-                    AfterResetAsync,
-                    "Are you sure you want to reset the count?",
-                    "Didn't get that!",
-                    promptStyle: PromptStyle.Auto);
+                case "version":
+                    response = $"{count++}: Version 0.0.0.1-alpha";
+                    break;
+                case "pancakes":
+                    if (askedAboutPancakes != null)
+                    {
+                        response = $"{count++}: You've asked about pancakes {askedAboutPancakes}.";
+                    }
+                    else
+                    {
+                        PromptDialog.Confirm(
+                            context,
+                            AfterPancakeAsync,
+                            "do you like pancakes?",
+                            "What?!",
+                            promptStyle: PromptStyle.Auto);
+                        askedAboutPancakes = DateTime.Now.ToString();
+                    }
+                    break;
+                
+                case "reset":
+                    PromptDialog.Confirm(
+                        context,
+                        AfterResetAsync,
+                        "Are you sure you want to reset the count?",
+                        "Didn't get that!",
+                        promptStyle: PromptStyle.Auto);
+                    break;
+                default:
+                    response = $"{count++}: You said {message.Text}";
+                    break;
             }
-            else
+            // Send standard response
+            if (response != null)
             {
-                await context.PostAsync($"{this.count++}: You said {message.Text}");
+                await context.PostAsync($"{count++}: You said {message.Text}");
                 context.Wait(MessageReceivedAsync);
             }
         }
 
-        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        private async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
         {
             var confirm = await argument;
             if (confirm)
             {
-                this.count = 1;
+                count = 1;
                 await context.PostAsync("Reset count.");
             }
             else
@@ -53,5 +83,19 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             context.Wait(MessageReceivedAsync);
         }
 
+        private async Task AfterPancakeAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                count = 1;
+                await context.PostAsync("I also like them.");
+            }
+            else
+            {
+                await context.PostAsync("Still, I like them.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
     }
 }
